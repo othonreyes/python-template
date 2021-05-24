c2f2b30f47d048f1fbb946883d54d7136f17a8c7
Othon R <othon.reyes@gmail.com>
2021-05-23 19:20:38 -0700

    add a sample module

--- test/test_sample.py
+++
@@ -1,5 +0,0 @@
-import hello
-
-
-def test_answer():
-    hello.print_loop()
---
+++ tests/test_simple.py
@@ -0,0 +1,20 @@
+# the inclusion of the tests module is not meant to offer best practices for
+# testing in general, but rather to support the `find_packages` example in
+# setup.py that excludes installing the "tests" package
+
+import unittest
+
+import hello
+from sample.simple import add_one
+
+
+class TestSimple(unittest.TestCase):
+    def test_add_one(self):
+        self.assertEqual(add_one(5), 6)
+
+    def test_answer(self):
+        hello.print_loop()
+
+
+if __name__ == "__main__":
+    unittest.main()
